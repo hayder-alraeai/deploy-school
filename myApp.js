fa7be29f32f4c9b3ca2b5535329839557864bab7
hayder86 <hayder@andrepeat.io>
2021-11-21 19:46:32 +0100

    deploy

--- myApp.js
+++ myApp.js
@@ -1,1 +1,11 @@
-console.log("test deploy");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello doqi");
+});
+
+app.listen(3004, () => {
+  console.log("app listning on port 3004");
+});
